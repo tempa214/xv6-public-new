2d970c22da57fdb83ad7c4346ecc9d26a3404004
Ganesh <108715534+tempa214@users.noreply.github.com>
2023-02-20 18:26:20 +0530

    Update proc.h

--- proc.h
+++ proc.h
@@ -49,6 +49,7 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  int time_scheduled; // time process has been scheduled in ticks
 };
 
 // Process memory is laid out contiguously, low addresses first:
@@ -56,11 +57,3 @@ struct proc {
 //   original data and bss
 //   fixed-size stack
 //   expandable heap
-
-//new lines
-
-struct proc {
-  ...
-  int time_sched;  // Add this line for time scheduled
-  ...
-}
